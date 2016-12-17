0793a0eb66be8b5440995a9374cbc8fbe7e3f9fc
TheCryptek <thecryptek@outlook.com>
2016-12-17 09:10:25 -0500

    Added custom version of base items from the game to start with. Still modified for debugging and testing purposes.

--- media/lua/client/TCSpawner.lua
+++ media/lua/client/TCSpawner.lua
@@ -9,36 +9,49 @@ function TCSpawner.addItems(_keyPressed)
 	print(_keyPressed); --[ print the pressed key to the console.
 		
 	--[ We Test if the correct key is pressed.
-	if _keyPressed == 79 then
+	if _keyPressed == 79 then --[ NumPad 1
 		local player = getSpecificPlayer(0); --[ Java: get player one
 		local inv = player:getInventory(); --[ Java: access player's inventory
 	
 		--[ Java: add the actual items to the inventory
-		inv:AddItem("Base.HammerStone");
-		inv:AddItem("Base.AxeStone");
-		inv:AddItem("Base.Sledgehammer");
-		inv:AddItem("Base.Saw");
-		inv:AddItem("Base.NailsBox");
+		inv:AddItem("TCSpawner.Duffelbag"); --[ Give Custom Duffelbag
 	end
 	if _keyPressed == 80 then
 		local player = getSpecificPlayer(0);
 		local inv = player:getInventory();
 		
-		inv:AddItem("Base.Needle");
-		inv:AddItem("Base.Thread");
+		inv:AddItem("Base.ElectronicsMag4");
+		inv:AddItem("Base.Generator");
 	end
 	if _keyPressed == 81 then
 		local player = getSpecificPlayer(0);
 		local inv = player:getInventory();
 		
-		inv:AddItem("Base.Duffelbag");
+		inv:AddItem("Base.PetrolCan");
 	end
 	if _keyPressed == 75 then
 		local player = getSpecificPlayer(0);
 		local inv = player:getInventory();
 		
-		inv:AddItem("Base.Padlock");
-		inv:AddItem("Base.ElectronicsMag4");
+		inv:AddItem("Base.NailsBox");
+	end
+	if _keyPressed == 76 then
+		local player = getSpecificPlayer(0);
+		local inv = player:getInventory();
+	
+		inv:AddItem("TCSpawner.AxeStone");
+		inv:AddItem("TCSpawner.HuntingKnife");
+		inv:AddItem("TCSpawner.Hammer");
+		inv:AddItem("TCSpawner.Sledgehammer");
+		inv:AddItem("TCSpawner.Screwdriver");
+		inv:AddItem("Base.Saw");
+	end
+	if _keyPressed == 77 then
+		local player = getSpecificPlayer(0);
+		local inv = player:getInventory();
+		
+		inv:AddItem("TCSpawner.Shovel");
+		inv:AddItem("Base.WateredCan");
 	end
 end
 
